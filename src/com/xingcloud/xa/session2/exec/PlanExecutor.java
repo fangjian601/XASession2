38f2403abebf3dd733ee40b272319822e09719c5
方建 <superman601@126.com>
2013-02-20 19:23:06 +0800

    implement group

--- src/com/xingcloud/xa/session2/exec/PlanExecutor.java
+++ src/com/xingcloud/xa/session2/exec/PlanExecutor.java
@@ -2,13 +2,10 @@ package com.xingcloud.xa.session2.exec;
 
 import com.xingcloud.xa.session2.ra.Operation;
 import com.xingcloud.xa.session2.ra.RelationProvider;
-import com.xingcloud.xa.session2.ra.RowIterator;
 import com.xingcloud.xa.session2.ra.impl.AbstractOperation;
-import com.xingcloud.xa.session2.ra.impl.XRelation;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -35,29 +32,6 @@ public class PlanExecutor {
         for(AbstractOperation operation : inputs){
             operation.evaluate();
         }
-        printResult((XRelation)root.evaluate());
-		return null;
+		return root.evaluate();
 	}
-
-    public static void printResult(XRelation relation){
-        StringBuilder sb = new StringBuilder();
-        Map<String, Integer> columnIndex = relation.getColumnIndex();
-        String[] columns = new String[columnIndex.size()];
-        for(String column: columnIndex.keySet()){
-            columns[columnIndex.get(column)] = column;
-        }
-        for(String column: columns){
-            sb.append(column+"\t");
-        }
-        sb.append("\n");
-        RowIterator iterator = relation.iterator();
-        while(iterator.hasNext()){
-            XRelation.XRow row = (XRelation.XRow) iterator.nextRow();
-            for(Object data: row.rowData){
-                sb.append(data+"\t");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
-    }
 }
--- src/com/xingcloud/xa/session2/ra/impl/XGroup.java
+++ src/com/xingcloud/xa/session2/ra/impl/XGroup.java
@@ -1,11 +1,15 @@
 package com.xingcloud.xa.session2.ra.impl;
 
-import com.xingcloud.xa.session2.ra.Group;
-import com.xingcloud.xa.session2.ra.Relation;
-import com.xingcloud.xa.session2.ra.RelationProvider;
+import com.xingcloud.xa.session2.ra.*;
 import com.xingcloud.xa.session2.ra.expr.Expression;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Author: mulisen
@@ -21,7 +25,41 @@ public class XGroup extends AbstractOperation implements Group {
 	Expression[] projectionExpressions;
 
 	public Relation evaluate() {
-		return null;  //TODO method implementation
+        if(result == null){
+            RowIterator iterator = relation.iterator();
+            Map<String, List<Object[]>> groups = new HashMap<String, List<Object[]>>();
+            while(iterator.hasNext()){
+                XRelation.XRow row = (XRelation.XRow)iterator.nextRow();
+                StringBuilder key = new StringBuilder();
+                for(Expression expr: groupingExpressions){
+                    key.append(getMD5(expr.evaluate(row).toString()));
+                }
+                if(groups.containsKey(key.toString())){
+                    List<Object[]> rows = groups.get(key.toString());
+                    rows.add(row.rowData);
+                } else {
+                    List<Object[]> rows = new ArrayList<Object[]>();
+                    rows.add(row.rowData);
+                    groups.put(key.toString(), rows);
+                }
+            }
+            List<Object[]> rows = new ArrayList<Object[]>();
+            Map<String, Integer> columnIndex = null;
+            for(List<Object[]> groupRows: groups.values()){
+                XRelation groupRelation = new XRelation(relation.getColumnIndex(), groupRows);
+                Projection projection = PlanFactory.getInstance().newProjection();
+                projection.setInput(groupRelation, projectionExpressions);
+                Relation projectionRelation = projection.evaluate();
+                if(columnIndex == null) columnIndex = projectionRelation.getColumnIndex();
+                RowIterator projectionIterator = projectionRelation.iterator();
+                if(projectionIterator.hasNext()){
+                    XRelation.XRow projectionRow = (XRelation.XRow) projectionIterator.nextRow();
+                    rows.add(projectionRow.rowData);
+                }
+            }
+            result = new XRelation(columnIndex, rows);
+        }
+		return result;
 	}
 
     public Group setInput(RelationProvider relation, Expression[] groupingExpressions, Expression[] projectionExpressions) {
@@ -37,5 +75,23 @@ public class XGroup extends AbstractOperation implements Group {
 	public String toString() {
 		return IndentPrint.print(this);
 	}
+
+    private String getMD5(String input){
+        MessageDigest m = null;
+        try {
+            m = MessageDigest.getInstance("MD5");
+            m.reset();
+            m.update(input.getBytes());
+            byte[] digest = m.digest();
+            BigInteger bigInt = new BigInteger(1,digest);
+            String hashText = bigInt.toString(16);
+            while(hashText.length() < 32 ){
+                hashText = "0"+hashText;
+            }
+            return hashText;
+        } catch (NoSuchAlgorithmException e) {
+            return "";
+        }
+    }
 
 }
