fb250670c44c7cce9242db2a51ad297ed03d147f
方建 <superman601@126.com>
2013-02-20 17:23:03 +0800

    implement PlanExecutor

--- src/com/xingcloud/xa/session2/exec/PlanExecutor.java
+++ src/com/xingcloud/xa/session2/exec/PlanExecutor.java
@@ -2,6 +2,14 @@ package com.xingcloud.xa.session2.exec;
 
 import com.xingcloud.xa.session2.ra.Operation;
 import com.xingcloud.xa.session2.ra.RelationProvider;
+import com.xingcloud.xa.session2.ra.RowIterator;
+import com.xingcloud.xa.session2.ra.impl.AbstractOperation;
+import com.xingcloud.xa.session2.ra.impl.XRelation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * Author: mulisen
@@ -10,8 +18,46 @@ import com.xingcloud.xa.session2.ra.RelationProvider;
 public class PlanExecutor {
 
 	public static RelationProvider executePlan(Operation root){
-		System.out.println("root = " + root);
-		//TODO 执行root为根节点的Plan，返回执行结果
+        List<AbstractOperation> inputs = new ArrayList<AbstractOperation>();
+        Stack<RelationProvider> tempStack = new Stack<RelationProvider>();
+        tempStack.push(root);
+        while(!tempStack.empty()){
+            RelationProvider relationProvider = tempStack.pop();
+            if(relationProvider instanceof AbstractOperation){
+                if(((AbstractOperation) relationProvider).getInputs().size() == 0){
+                    inputs.add((AbstractOperation) relationProvider);
+                }
+                for(int i=0; i<((AbstractOperation) relationProvider).getInputs().size(); i++){
+                    tempStack.push(((AbstractOperation) relationProvider).getInputs().get(i));
+                }
+            }
+        }
+        for(AbstractOperation operation : inputs){
+            operation.evaluate();
+        }
+        printResult((XRelation)root.evaluate());
 		return null;
 	}
+
+    public static void printResult(XRelation relation){
+        StringBuilder sb = new StringBuilder();
+        Map<String, Integer> columnIndex = relation.getColumnIndex();
+        String[] columns = new String[columnIndex.size()];
+        for(String column: columnIndex.keySet()){
+            columns[columnIndex.get(column)] = column;
+        }
+        for(String column: columns){
+            sb.append(column+"\t");
+        }
+        sb.append("\n");
+        RowIterator iterator = relation.iterator();
+        while(iterator.hasNext()){
+            XRelation.XRow row = (XRelation.XRow) iterator.nextRow();
+            for(Object data: row.rowData){
+                sb.append(data+"\t");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
